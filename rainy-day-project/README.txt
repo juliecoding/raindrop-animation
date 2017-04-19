0bd809b1544579199e08e1b2aeb2ca17247c815a
juliecoding <julie.akohler@gmail.com>
2017-04-19 14:55:40 -0600

    First commit

---
+++ rainy-day-project/README.txt
@@ -0,0 +1,5 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/jakohler/pen/KmVKNM.
+
+ A chance for me to practice aleatory animation  :) 
+
+The raindrop SVG was based on this video: https://www.youtube.com/watch?v=8g9F_nCBWro 
---
+++ rainy-day-project/js/index.js
@@ -0,0 +1,14 @@
+var newDiv = document.createElement('div');
+document.body.appendChild(newDiv);
+newDiv.style.width = "500px"; 
+newDiv.style.height = "200px"; 
+newDiv.style.border = "white solid"; 
+newDiv.style.borderWidth = "1px";
+
+//Read more: http://mrbool.com/how-to-add-elements-html-dynamically-with-javascript/26752#ixzz4efROV5T1
+
+
+
+//Things to do next: get the SVG actually looking proper. 
+
+//
